5fccfdad71f9a67f25c1153ab01386b6d94e386c
Birat <biratdey200311@gmail.com>
2026-08-07 23:10:40 +0530

    Add 1sttest.py for numeric data type demonstration and type checking examples

---
+++ DataTypes_&_Variables/DataTypes/1sttest.py
@@ -0,0 +1,30 @@
+# Numeric show case
+var1 = 1        # int data type
+var2 = True     # bool data type
+var3 = 10.83    # float data type
+var4 = 10+3j    # complex data type
+
+# 1st test case on Numeric -->
+x = int(867_5309)
+print(x)
+
+# Example -->
+#integer value
+a = 19
+print("The type of variable having value", a, " is ", type(a))
+
+#float variable
+c = 19.45
+print("The type of variable having value", a, " is ", type(c))
+
+#complex variable
+e = 19+8j
+print("The type of varible having value", e, " is ", type(e))
+
+
+# Output look like this -->
+"""
+The type of variable having value 19  is  <class 'int'>
+The type of variable having value 19  is  <class 'float'>
+The type of varible having value (19+8j)  is  <class 'complex'>
+"""
