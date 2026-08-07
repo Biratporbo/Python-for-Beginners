c17d79ea37d014ae3b8693a2e17355d4911ac2dc
Birat <biratdey200311@gmail.com>
2026-08-07 23:01:09 +0530

    Add python test file

---
+++ Learn The Basics/test.py
@@ -0,0 +1,22 @@
+# At First Print the statment -->
+# Return value-->
+print ("Hello World!")
+
+# Comments -->
+'''This is comment method to used'''
+# also this is too comment can use
+name = input("What is your name?")
+print("Hello,")
+print(name)
+
+# Pseudocode -->
+# ask for there name
+name = input("How about you?")
+# give to answer to the user
+print("Hi!")
+print(name)
+
+# Extract add used -->
+name = input("What are you doing?")
+# give to answer
+print("Hello,", name)
