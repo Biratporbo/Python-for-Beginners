9b3527c90951ffb96b916dec79d52d241fc0b4fa
Birat <biratdey200311@gmail.com>
2026-08-08 22:33:19 +0530

    Add 8thtest.py to demonstrate None type and variable type assignments

---
+++ DataTypes_&_Variables/DataTypes/8thtest.py
@@ -0,0 +1,48 @@
+# i. None Type Show case
+#Example -->
+
+# Declaring a variable
+# And, assigning a Null value (None)
+
+x = None
+# Printing its values and types
+print("x = ", x)
+print("type of x = ", type(x))
+
+
+# ii. Getting Data Type --->
+# Getting type of values
+print(type(123))
+print(type(9.99))
+
+# Getting type of variables
+a = 10
+b = 2.12
+c = "Hello"
+d = (10, 20, 30)
+e = [10, 20, 30]
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+# iii. Setting Data Type 
+# Declaring a variable
+# And, assigning an integer value
+
+x = 10
+
+# Printing its value and type
+print("x = ", x)
+print("type of x = ", type(x))
+
+# Now, assigning string value to the same variable
+x = "Hi Everyone!"
+
+# Printing its value and type
+print("x = ", x)
+print("type of x = ", type(x))
+
