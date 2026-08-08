b3143d83b57534ea5e6481e9f0b7770b4f416220
Birat <biratdey200311@gmail.com>
2026-08-08 21:45:52 +0530

    Add 6thtest.py to demonstrate Set data type with examples

---
+++ DataTypes_&_Variables/DataTypes/6thtest.py
@@ -0,0 +1,12 @@
+# Set Data Type Show case --->
+# >>> {2023, "Python", 3.11, 5+6j, 1.23E-4}
+# {(5+6j), 3.11, 0.000123, 'Python', 2023}
+
+# >>> type({2023, "Python", 3.11, 5+6j, 1.23E-4}) <--- <class 'set'>
+
+# Example of Set:
+set1 = {123, 199, 89, 98}
+set2 = {'Java', 'Python', 'JavaScript'}
+
+print(set1)
+print(set2)
