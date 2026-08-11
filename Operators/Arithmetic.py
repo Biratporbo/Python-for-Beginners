537c9e24effbddc88d0868ee84453dfd220b30ef
Birat <biratdey200311@gmail.com>
2026-08-11 15:45:07 +0530

    Add arithmetic operators example

---
+++ Operators/Arithmetic.py
@@ -0,0 +1,29 @@
+# Examples of Arithmetic Operator
+a = 10
+b = 9
+c = 0
+
+c = a + b
+print("a: {} b: {} a+b {}".format(a,b,c))
+
+c = a * b
+print("a: {} b: {} a*b {}".format(a,b,c))
+
+c = a - b
+print("a: {} b: {} a-b {}".format(a,b,c))
+
+c = a/b
+print("a: {} b: {} a/b: {}".format(a,b,c))
+
+c = a % b
+print("a: {} b: {} a%b: {}".format(a,b,c))
+
+a = 9
+b = 2
+c = a**b
+print("a: {} b: {} a**b: {}".format(a,b,c))
+
+a = 10
+b = 5
+c = a//b 
+print ("a: {} b: {} a//b: {}".format(a,b,c))
